8e78f697046c08df82a2a80e1119cb495e18742e
codewesleylima <gusterlima@hotmail.com>
2026-05-25 22:51:00 -0300

    chore: Remove all Java classes - keep folder structure and infrastructure

--- src/main/java/com/wzzy/library/msauth/exception/UserAlreadyExistsException.java
+++
@@ -1,9 +0,0 @@
-package com.wzzy.library.msauth.exception;
-
-import com.wzzy.library.bfflibrary.exception.BaseServiceException;
-
-public class UserAlreadyExistsException extends BaseServiceException {
-    public UserAlreadyExistsException(String field, String value) {
-        super("User with " + field + " '" + value + "' already exists", "USER_ALREADY_EXISTS", 409);
-    }
-}
